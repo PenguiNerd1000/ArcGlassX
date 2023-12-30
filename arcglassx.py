cbf1db11b8b960bd1266ff132f6b39cfd8649546
Jovanson Soedomo <jovanson@c02fm6kkml7h.lan>
2023-12-30 22:58:25 +0900

    image cropping method, updated requirements (still unsure if correct libs)

--- arcglassx.py
+++ arcglassx.py
@@ -11,7 +11,6 @@ engine = pyttsx3.init()
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\rayz1\AppData\Local\Tesseract-OCR\tesseract.exe'
 
-
 def recognize_handwritten(image):
     text = pytesseract.image_to_string(image)
     return text
@@ -25,15 +24,11 @@ def recognize_printed(image):
     return corrected_text
 
 def is_handwritten(image):
-
-
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
     edged = cv2.Canny(blurred, 30, 150)
 
-
     contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
 
     is_handwritten = False
     for contour in contours:
@@ -45,25 +40,22 @@ def is_handwritten(image):
         else:
             return False
 
-
-
 videocapture = cv2.VideoCapture(0)
-
+roi_coords = (0, 0, videocapture.get(3), videocapture.get(4))
 
 while True:
-
     _, frame = videocapture.read()
+    
+    x, y, w, h = roi_coords
+    roi_frame = frame[y:y+h, x:x+w]
 
-   
-    if is_handwritten(frame):
-        text = recognize_handwritten(frame)
+    if is_handwritten(roi_frame):
+        text = recognize_handwritten()
         print("Handwritten")
     else:
         text = recognize_printed(frame)
         print("printed")
 
-    text = correct_spelling(text)
-        #speak
     if text:
         engine.say(text)
         engine.runAndWait()
@@ -72,10 +64,11 @@ while True:
         #show frame
     cv2.imshow('Video', frame)
 
+    y += int(videocapture.get(4)/10)
+
     #stop when input q is inputted
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-
-
 videocapture.release()
+cv2.destroyAllWindows()
